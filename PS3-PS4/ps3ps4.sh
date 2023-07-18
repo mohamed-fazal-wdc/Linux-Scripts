674f780672ec18a95898b673230c092120eac73e
Mohamed Farhan Fazal <mohamed.fazal@wdc.com>
2023-07-18 18:28:06 +0530

    grabs proper values

--- PS3-PS4/ps3ps4.sh
+++ PS3-PS4/ps3ps4.sh
@@ -8,10 +8,8 @@ fi
 
 log_file="PS3_PS4_$(date +%Y%m%d%H%M%S).log"
 
-# Run nvme list and prompt user to select the NVMe drive
-echo "Available NVMe drives:"
-nvme list
-echo -n "Enter the number corresponding to the NVMe drive: "
+# Run output=$);value=$(echo "$output" | awk -F', ' '{split($2, arr, ":"); print arr[2]}');echo "Extracted value: $value"
+echo -n);value=$(echo "$output" | awk -F', ' '{split($2, arr, ":"); print arr[2]}');echo "Extracted value: $value"
 read nvme_number
 
 nvme_device="/dev/nvme${nvme_number}"
@@ -29,9 +27,9 @@ do
     date +%m-%d-%H-%M-%S
     date +%m-%d-%H-%M-%S >> "$log_file"
     echo "Running Set-Features"
-    nvme set-feature "$nvme_device" -f 0x02 -v 0x03 | awk -F'Current value:' '{print $2}'
+    output=$(nvme set-feature "$nvme_device" -f 0x02 -v 0x03);value=$(echo "$output" | awk -F', ' '{split($2, arr, ":"); print arr[2]}');echo "Extracted value: $value"
     echo "Running Get-Features"
-    nvme get-feature "$nvme_device" -f 0x02 -s 0x00 | awk -F'Current value:' '{print $2}'
+    output=$(nvme get-feature "$nvme_device" -f 0x02 -s 0x00);value=$(echo "$output" | awk -F', ' '{split($2, arr, ":"); print arr[2]}');echo "Extracted value: $value"
     sleep 0.03
 
     # PS3 Exit
@@ -39,9 +37,9 @@ do
     date +%m-%d-%H-%M-%S
     date +%m-%d-%H-%M-%S >> "$log_file"
     echo "Running Set-Features"
-    nvme set-feature "$nvme_device" -f 0x02 -v 0x00 | awk -F'Current value:' '{print $2}'
+    output=$(nvme set-feature "$nvme_device" -f 0x02 -v 0x00);value=$(echo "$output" | awk -F', ' '{split($2, arr, ":"); print arr[2]}');echo "Extracted value: $value"
     echo "Running Get-Features"
-    nvme get-feature "$nvme_device" -f 0x02 -s 0x00 | awk -F'Current value:' '{print $2}'
+    output=$(nvme get-feature "$nvme_device" -f 0x02 -s 0x00);value=$(echo "$output" | awk -F', ' '{split($2, arr, ":"); print arr[2]}');echo "Extracted value: $value"
     sleep 0.06
 
     echo "PS4 in = 30ms out = 60ms | Cycles = $num_cycles | Current Loop = $i"
@@ -52,9 +50,9 @@ do
     date +%m-%d-%H-%M-%S
     date +%m-%d-%H-%M-%S >> "$log_file"
     echo "Running Get-Features"
-    nvme set-feature "$nvme_device" -f 0x02 -v 0x04 | awk -F'Current value:' '{print $2}'
+    output=$(nvme set-feature "$nvme_device" -f 0x02 -v 0x04);value=$(echo "$output" | awk -F', ' '{split($2, arr, ":"); print arr[2]}');echo "Extracted value: $value"
     echo "Running Get-Features"
-    nvme get-feature "$nvme_device" -f 0x02 -s 0x00 | awk -F'Current value:' '{print $2}'
+    output=$(nvme get-feature "$nvme_device" -f 0x02 -s 0x00);value=$(echo "$output" | awk -F', ' '{split($2, arr, ":"); print arr[2]}');echo "Extracted value: $value"
     sleep 0.030
 
     # PS4 Exit
@@ -62,8 +60,8 @@ do
     date +%m-%d-%H-%M-%S
     date +%m-%d-%H-%M-%S >> "$log_file"
     echo "Running Get-Features"
-    nvme set-feature "$nvme_device" -f 0x02 -v 0x00 | awk -F'Current value:' '{print $2}'
+    output=$(nvme set-feature "$nvme_device" -f 0x02 -v 0x00);value=$(echo "$output" | awk -F', ' '{split($2, arr, ":"); print arr[2]}');echo "Extracted value: $value"
     echo "Running Get-Features"
-    nvme get-feature "$nvme_device" -f 0x02 -s 0x00 | awk -F'Current value:' '{print $2}'
+    output=$(nvme get-feature "$nvme_device" -f 0x02 -s 0x00);value=$(echo "$output" | awk -F', ' '{split($2, arr, ":"); print arr[2]}');echo "Extracted value: $value"
     sleep 0.060
 done
